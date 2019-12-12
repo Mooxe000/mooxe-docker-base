411aac4c105db3de4093ed681e478c320d09cac2
FooTearth <footearth@gmail.com>
2019-12-12 20:33:23 +0800

    update server

--- server/init.sh
+++ server/init.sh
@@ -4,14 +4,15 @@ apt-get install -y curl apt-utils
 
 apt-get install -y software-properties-common && \
 add-apt-repository ppa:git-core/ppa && \
-apt-add-repository ppa:fish-shell/release-2
+apt-add-repository ppa:fish-shell/release-2 && \
+apt-add-repository ppa:apt-fast/stable
 
 apt-get update && \
 apt-get -y upgrade && \
 apt-get autoremove -y
 
-apt-get -y install aria2 wget && \
-/bin/bash -c "$(curl -sL https://git.io/vokNn)"
+apt-get -y install aria2 wget apt-fast
+# /bin/bash -c "$(curl -sL https://git.io/vokNn)"
 
 apt-fast update && \
 apt-fast -y upgrade
